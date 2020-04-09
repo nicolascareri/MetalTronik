797634cedff6ed71b5640486695cae2672260226
Nicolás <nicolascareri@gmail.com>
2020-04-09 11:47:24 -0300

    Intregado material design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSliderModule } from "@angular/material/slider";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserService } from './user.service';
 import { HttpClientModule} from '@angular/common/http';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { UsuarioComponent } from './usuario/usuario.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
