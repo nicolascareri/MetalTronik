133da5aaf56a975c7b6cb83e0ce71f5a0f9279a4
Nicolás <nicolascareri@gmail.com>
2020-04-22 10:58:14 -0300

    change name package and @valid in maquina controller

--- backend/src/main/java/com/example/metalTest/maquina/controller/request/MaquinaRequest.java
+++ backend/src/main/java/com/example/metalTest/maquina/controller/request/MaquinaRequest.java
@@ -1,11 +1,15 @@
-package com.example.metalTest.maquina.controller.response;
+package com.example.metalTest.maquina.controller.request;
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class MaquinaRequest {
+    @NotBlank
     private String maquina_cod;
+    @NotBlank
     private String sector;
 }
