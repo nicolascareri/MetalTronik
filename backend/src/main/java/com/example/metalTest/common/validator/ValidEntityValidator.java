88c973b488ba41fa18dc13173aac0deb0de88c88
Nicolás <nicolascareri@gmail.com>
2020-05-21 14:57:14 -0300

    fixed

---
+++ backend/src/main/java/com/example/metalTest/common/validator/ValidEntityValidator.java
@@ -0,0 +1,32 @@
+package com.example.metalTest.common.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ValidEntityValidator implements ConstraintValidator<ValidEntity, Object> {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private JpaRepository repository;
+
+    private boolean required;
+
+    @Override
+    public void initialize(ValidEntity constraintAnnotation) {
+        repository = applicationContext.getBean(constraintAnnotation.repository());
+        required = constraintAnnotation.required();
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        if (o != null){
+            return repository.existsById(o);
+        }
+        else return !required;
+    }
+}
--- backend/src/main/java/com/example/metalTest/ordenestrabajo/controller/request/OrdenesTrabajoRequest.java
+++ backend/src/main/java/com/example/metalTest/ordenestrabajo/controller/request/OrdenesTrabajoRequest.java
@@ -1,5 +1,7 @@
 package com.example.metalTest.ordenestrabajo.controller.request;
-
+import com.example.metalTest.common.validator.ValidEntity;
+import com.example.metalTest.sector.repository.SectorRepository;
+import com.example.metalTest.usuario.repository.UsuarioRepository;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,14 +35,17 @@ public class OrdenesTrabajoRequest {
     @NotNull
     private Date fechaEntrega;
     @NotNull
+    @ValidEntity(repository = UsuarioRepository.class)
     private int encargo_cod;
     @NotNull
+    @ValidEntity(repository = UsuarioRepository.class)
     private int responsable_cod;
     @NotNull
     @Min(1)
     @Max(2)
     private short estado;
     @NotNull
+    @ValidEntity(repository = SectorRepository.class)
     private int sector_cod;
 
     private String observaciones;
