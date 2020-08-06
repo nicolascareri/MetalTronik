68bd92c69a9c8ba759f6326684b2b3d6a1ef44c9
FacaRodri <facundo.eseverri@gmail.com>
2020-08-06 09:56:27 -0300

    [Refact] Seccion repuestos

--- frontend/src/app/main/components/sidenav/sidenav.component.ts
+++ frontend/src/app/main/components/sidenav/sidenav.component.ts
@@ -19,7 +19,7 @@ export class SidenavComponent implements OnDestroy {
     {name: 'Ordenes', route: 'ordenes', icon: 'calendar_today'},
     {name: 'Mantenimiento Correctivo', route: 'mantenimientosCorrectivos', icon: 'assignment'},
     {name: 'Maquinas', route: 'maquinas', icon: 'build'},
-    {name: 'Repuestos de maquinas', route: 'repuestos', icon: 'build_circle'},
+    {name: 'Repuestos', route: 'repuestos', icon: 'build_circle'},
     {name: 'Usuarios', route: 'usuarios', icon: 'recent_actors'},
     {name: 'Indicadores', route: '', icon: 'assessment'},
     {name: 'Configuracion', route: 'configuracion', icon: 'settings'}
