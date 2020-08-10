9235091416c1ae97333af115f18121ffdd071772
FacaRodri <facundo.eseverri@gmail.com>
2020-08-10 18:19:40 -0300

    Borrado consol log

--- frontend/src/app/repuesto-maquina/components/lista-repuestos/lista-repuestos.component.ts
+++ frontend/src/app/repuesto-maquina/components/lista-repuestos/lista-repuestos.component.ts
@@ -62,7 +62,7 @@ export class ListaRepuestosComponent implements OnInit {
   }
 
   asociar() {
-    this.RepuestoMaquinaService.asociarRepuestos(this.maquinaId, this.repuestos).subscribe(repuestos => alert(`Exito: ${JSON.stringify(repuestos)}`));
+    this.RepuestoMaquinaService.asociarRepuestos(this.maquinaId, this.repuestos).subscribe(repuestos => alert("Exitos:" + repuestos));
   }
 
 
@@ -112,7 +112,6 @@ export class ListaRepuestosComponent implements OnInit {
    
   //  console.log(this.modeloValue);
    console.log(m);   
-   this.seleccionRepuesto = 'all';
   }
 
   getRepuestos(){
