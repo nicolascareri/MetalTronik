410280b963b7db81691b63f097f59850756b0a9f
FacaRodri <facundo.eseverri@gmail.com>
2020-09-07 12:05:40 -0300

    Changed sidenav to navbar

--- frontend/src/app/main/components/sidenav/sidenav.component.ts
+++ frontend/src/app/main/components/sidenav/sidenav.component.ts
@@ -1,5 +1,4 @@
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
-import {MediaMatcher} from '@angular/cdk/layout';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,36 +7,21 @@ import {MediaMatcher} from '@angular/cdk/layout';
 })
 
 
-export class SidenavComponent implements OnDestroy {
-
-
+export class SidenavComponent{
 
   mobileQuery: MediaQueryList;
 
   fillerNav = [
-    {name: 'Inicio', route: '', icon: 'home'},
-    {name: 'Ordenes', route: 'ordenes', icon: 'calendar_today'},
-    {name: 'Mantenimiento Correctivo', route: 'mantenimientosCorrectivos', icon: 'assignment'},
-    {name: 'Maquinas', route: 'maquinas', icon: 'build'},
-    {name: 'Repuestos', route: 'repuestos', icon: 'build_circle'},
-    {name: 'Usuarios', route: 'usuarios', icon: 'recent_actors'},
-    {name: 'Indicadores', route: '', icon: 'assessment'},
-    {name: 'Configuracion', route: 'configuracion', icon: 'settings'}
+    { name: 'Inicio', route: '', icon: 'home' },
+    { name: 'Ordenes', route: 'ordenes', icon: 'calendar_today' },
+    { name: 'Mantenimiento Correctivo', route: 'mantenimientosCorrectivos', icon: 'assignment' },
+    { name: 'Maquinas', route: 'maquinas', icon: 'build' },
+    { name: 'Repuestos', route: 'repuestos', icon: 'build_circle' },
+    { name: 'Usuarios', route: 'usuarios', icon: 'recent_actors' },
+    { name: 'Indicadores', route: '', icon: 'assessment' },
+    { name: 'Configuracion', route: 'configuracion', icon: 'settings' }
   ]
 
 
-  private _mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-
-  shouldRun = true;
-
+ 
 }
