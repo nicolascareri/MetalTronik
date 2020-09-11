7fda97b1960d45261aa4aaea1fbbe973a9729761
FacaRodri <facundo.eseverri@gmail.com>
2020-09-11 10:45:55 -0300

    Generated almacen module

---
+++ frontend/src/app/almacen/almacen-rounting.module.ts
@@ -0,0 +1,64 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormMaquinaComponent} from 'src/app/maquina/components/form/form.component';
+import {TablaMaquinaComponent} from 'src/app/maquina/components/tabla/tabla.component';
+import {FormSectorComponent} from '../sector/components/form/form.component';
+import {FormPlantaComponent} from '../planta/components/form/form.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: TablaMaquinaComponent
+      },
+      {
+        path: 'form',
+        component: FormMaquinaComponent
+      },
+      {
+        path: 'formsector',
+        component: FormSectorComponent
+      },
+      {
+        path: 'formplanta',
+        component: FormPlantaComponent
+      },
+      {
+        path: 'form/:id',
+        component: FormMaquinaComponent
+      },
+      {
+        path: 'formsector/:id',
+        component: FormSectorComponent
+      },
+      {
+        path: 'formplanta/:id',
+        component: FormPlantaComponent
+      }
+
+    ]
+    }
+];
+
+
+@NgModule ({
+     imports: [
+
+         RouterModule.forChild(routes)
+
+     ],
+     exports:[
+
+        RouterModule
+
+     ]
+
+})
+
+
+export class AlmacenRoutingModule{
+
+}
--- frontend/src/app/main/components/sidenav/sidenav.component.ts
+++ frontend/src/app/main/components/sidenav/sidenav.component.ts
@@ -19,6 +19,7 @@ export class SidenavComponent{
     { name: 'Ordenes', route: 'ordenes', icon: 'calendar_today' },
     { name: 'Mantenimiento Correctivo', route: 'mantenimientosCorrectivos', icon: 'assignment' },
     { name: 'Maquinas', route: 'maquinas', icon: 'build' },
+    { name: 'Almacen', route: 'almacen', icon: 'settings' },
     { name: 'Repuestos', route: 'repuestos', icon: 'build_circle' },
     { name: 'Usuarios', route: 'usuarios', icon: 'recent_actors' },
     { name: 'Indicadores', route: '', icon: 'assessment' },
