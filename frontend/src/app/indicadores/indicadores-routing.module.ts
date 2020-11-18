117c1db3f10076e1fcb7376e3ac88ead374ef9ac
FacaRodri <facundo.eseverri@gmail.com>
2020-11-18 09:53:11 -0300

    Seccion indicadores in progress

---
+++ frontend/src/app/indicadores/indicadores-routing.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { MainComponent } from "../indicadores/components/main/main/main.component";
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: MainComponent
+      }
+    ]
+    }
+];
+@NgModule ({
+     imports: [
+
+         RouterModule.forChild(routes)
+
+     ],
+     exports:[
+
+        RouterModule
+
+     ]
+
+})
+
+export class IndicadoresRoutingModule{
+
+}
