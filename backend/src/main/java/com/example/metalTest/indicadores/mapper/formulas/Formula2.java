2b286a1bbb764780f15b26e7733aa0392f2426c1
Alan <alanyd.phh@gmail.com>
2020-11-19 12:04:18 -0300

    Indicadores formula 2 in progress

--- backend/src/main/java/com/example/metalTest/indicadores/mapper/formulas/Formula2.java
+++ backend/src/main/java/com/example/metalTest/indicadores/mapper/formulas/Formula2.java
@@ -1,51 +1,57 @@
 package com.example.metalTest.indicadores.mapper.formulas;
 
 import com.example.metalTest.indicadores.controller.response.IndicatorResponse;
-import com.example.metalTest.indicadores.mapper.ToIndicadoresMapper;
-import com.example.metalTest.mantenimientoCorrectivo.domain.MantenimientoCorrectivo;
 import com.example.metalTest.ordenestrabajo.domain.OrdenesTrabajo;
 import org.springframework.stereotype.Component;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @Component
-public class Formula2  extends ToIndicadoresMapper implements Formula {
+public class Formula2  extends Formula {
 
     public Formula2() {
     }
 
     @Override
     public List<IndicatorResponse> getResultadoUsuario(List<OrdenesTrabajo> consulta) {
-        return getIndicadoresFormula2(consulta);
+        return getIndicadoresUsuario(consulta);
     }
 
     @Override
     public List<IndicatorResponse> getResultadoSector(List<OrdenesTrabajo> consult) {
+        return getIndicadoresSector(consult);
+    }
+
+    @Override
+    public IndicatorResponse getUsuario(List<OrdenesTrabajo> ordenes) {
         return null;
     }
 
-    private List<IndicatorResponse> getIndicadoresFormula2(List<OrdenesTrabajo> consulta) {
-        List<IndicatorResponse> a = new ArrayList<>();
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-            Date fechaFin = sdf.parse("2020-11-12");
-            Date fechaEntrega = sdf.parse("2019-11-12");
-            long diffInMillies = Math.abs(fechaEntrega.getTime() - fechaFin.getTime());
-            long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
-            System.out.println("daleeeee"+ diff);
-        }catch (Exception e){
-            System.out.println( "alto error pa: " +e);
-        }
-        return a;
+    @Override
+    IndicatorResponse getSector(List<OrdenesTrabajo> ordenes) {
+        return null;
     }
 
+    @Override
+    int getFormula(int a, int b) {
+        return a/b;
+    }
+
+
+    private int getDias(String fin, String entrega){
+        long diff = 0;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            Date fechaFin = sdf.parse(fin);
+            Date fechaEntrega = sdf.parse(entrega);
+            long diffInMillies = Math.abs(fechaEntrega.getTime() - fechaFin.getTime());
+            diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return 1;
+    }
 
 
 
